9e3f72103e1b861091a9168bf84fdae6b10d24d4
buyflow <122653408+Buy-Flow@users.noreply.github.com>
2026-09-03 14:20:52 -0400

    Forçar atualização do PWA ao abrir

--- calculadora/pwa-register.js
+++ calculadora/pwa-register.js
@@ -1,7 +1,18 @@
 (() => {
   if ("serviceWorker" in navigator) {
+    let refreshing = false;
+
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      if (refreshing) return;
+      refreshing = true;
+      window.location.reload();
+    });
+
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("./service-worker.js").catch(() => {});
+      navigator.serviceWorker
+        .register("./service-worker.js", { updateViaCache: "none" })
+        .then(registration => registration.update())
+        .catch(() => {});
     });
   }
 
